4917a44015d1f0cd97fbf389be856de5d7b0623f
fearschism <77707954+fearschism@users.noreply.github.com>
2021-05-30 20:24:39 +0300

    Create JsonDATA.ts
    this contains all the data of the user for the market

---
+++ JsonDATA.ts
@@ -0,0 +1,56 @@
+const number1:number=Math.floor(Math.random()*9999+1);//id generator
+var user = {
+    name:"user",
+    balance:1000,
+    Password:"user",
+    number:number1,
+    E_account = {
+        E_balance:0,
+        coins={
+            number_owned:0,
+            saadCoin={
+                number_owned_All:0,
+                shares:0
+            },
+            piCoin={
+                number_owned:0,
+                shares:0
+            },
+            byteCoin={
+                number_owned:0,
+                shares:0
+            },
+            tobiCoin={
+                number_owned:0,
+                shares:0
+            },
+            hashBrown={
+                number_owned:0,
+                shares:0
+            }
+        },
+        stocks={
+            number_owned_All:0,
+            GMR={
+                number_owned:0,
+                shares:0
+            },
+            muve={
+                number_owned:0,
+                shares:0
+            },
+            blkBucks={
+                number_owned:0,
+                shares:0
+            },
+            zaitco={
+                number_owned:0,
+                shares:0
+            },
+            gst={
+                number_owned:0,
+                shares:0
+            },
+        }
+    }
+}
